0d8aeb57bad3859fc7ab4a7759ac4d6b6b250b42
Jimmy Bradshaw <bradshjg@github.com>
2021-06-26 18:50:41 -0500

    diffs for changes still not working
    Hopefully this is a _bit_ closer though, doesn't seem to break anything

--- app/src/lib/fs/index.ts
+++ app/src/lib/fs/index.ts
@@ -17,10 +17,49 @@ export const repoPathExists = (repository: Repository, path: string): Promise<bo
 
 export const repoReadFile = (repository: Repository, path: string, encoding: string = 'utf-8'): Promise<string> => {
   if (repository.path.startsWith(virtualPrefix)) {
+    // HACK HACK HACK we need like a path that's not the protocol-y path, this is often joined in the code
     if (path.startsWith(virtualPrefix)) {
       path = path.replace(virtualPrefix, '')
     }
     return remoteFSE('readFile', [path, encoding], {})
   }
   return FSE.readFile(path, encoding)
+}
+
+/**
+ * Read a specific region from a file.
+ *
+ * @param path  Path to the file
+ * @param start First index relative to the start of the file to
+ *              read from
+ * @param end   Last index (inclusive) relative to the start of the
+ *              file to read to
+ */
+export async function repoReadPartialFile(
+  repository: Repository,
+  path: string,
+  start: number,
+  end: number
+): Promise<Buffer> {
+  return await new Promise<Buffer>((resolve, reject) => {
+    if (repository.path.startsWith(virtualPrefix)) {
+      if (path.startsWith(virtualPrefix)) {
+        path = path.replace(virtualPrefix, '')
+      }
+      repoReadFile(repository, path).then(value => {
+        resolve(Buffer.from(value.slice(start, end)))
+      })
+    }
+    else {
+      const chunks = new Array<Buffer>()
+      let total = 0
+
+      FSE.createReadStream(path, { start, end })
+        .on('data', (chunk: Buffer) => {
+          chunks.push(chunk)
+          total += chunk.length
+        })
+        .on('error', reject)
+        .on('end', () => resolve(Buffer.concat(chunks, total)))
+    }})
 }
